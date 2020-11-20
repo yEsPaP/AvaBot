18ba2661d1d5d039a1669dc981fd8a4883d1df97
D Avinash Kumar <56125142+davinash97@users.noreply.github.com>
2020-11-20 18:10:44 +0530

    Create covid.py

---
+++ avabot/modules/covid.py
@@ -0,0 +1,45 @@
+from covid import Covid
+from telegram import ParseMode, Update, Bot, Message, Chat
+from telegram.ext import run_async
+from avabot import dispatcher
+from avabot.modules.disable import DisableAbleCommandHandler
+
+def covid_19(bot, update, args: List[str])
+    msg = update.effective_message
+    covid = Covid(source="worldometers")
+    query = " ".join(args)
+    if not query:
+        msg.reply_text("Which Country You mean?")
+        return
+    else:
+        data = covid.get_status_by_country_name(args)
+        if data:
+            covid_data  = f"*Corona Virus Info:*\n"
+            covid_data += f"  *Country*          : `{data['country']}` \n"
+            covid_data += f"  *Confirmed*        : `{data['confirmed']}` \n"
+            covid_data += f"  *Death*            : `{data['deaths']}` \n"
+            covid_data += f"  *Active*           : `{data['active']}` \n"
+            covid_data += f"  *Recovered*        : `{data['recovered']}` \n\n"
+            covid_data += f"  *Critical Stage*   : `{data['critical']}` \n"
+            covid_data += f"  *Total*\n"
+            covid_data += f"  *New Cases*        : `{data['new_cases']}` \n"
+            covid_data += f"  *New Deaths*       : `{data['new_deaths']}` \n"
+            covid_data += f"  *Total Population* : `{data['population']}` \n\n"
+            covid_data += f"Data Provided by [*Worldometers*](https://www.worldometers.info/coronavirus)"
+            bot.send_message(chat_id = update.effective_chat.id,
+                             text=covid_data,
+                             parse_mode=ParseMode.MARKDOWN,
+                             disable_web_page_preview=True)
+        else:
+            msg.reply_text("Invalid Country")
+
+__help__ = """
+Need Covid-19 status?
+*Available Commands:*
+-/covid <country name>: To get you country's covid-19 status
+"""
+
+__mod_name__ = "Covid"
+
+COVID_HANDLER = DisableAbleCommandHandler("covid", covid_19, pass_args=True)
+dispatcher.add_handler(COVID_HANDLER)
